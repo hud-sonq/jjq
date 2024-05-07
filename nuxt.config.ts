44e6ae60cc11bde19eb0248bb1bd812b9eb14a1a
hud-sonq <hudson@koipond.xyz>
2024-05-07 11:50:21 -0400

    baseurl fix?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/global.css'],
   app: {
+    baseURL: '/jjq',
     pageTransition: { name: 'page', mode: 'out-in' }
   },
   ssr: true,
