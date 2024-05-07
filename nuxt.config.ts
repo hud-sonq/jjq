5e1c604993894915cf0ccd81c5e1c806fc83dc65
hud-sonq <hudson@koipond.xyz>
2024-05-07 11:58:50 -0400

    try again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/global.css'],
   app: {
-    baseURL: '/jjq',
     pageTransition: { name: 'page', mode: 'out-in' }
   },
   ssr: true,
