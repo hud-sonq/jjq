2f96278cdd168ae7fff8ddccecb09f8d98c62b36
hud-sonq <quatrohudson@gmail.com>
2026-05-12 17:27:57 -0400

    about into index

--- app.vue
+++ app.vue
@@ -1,55 +1,17 @@
 <template>
-  <NavBar class="nav" :class="{active: siteEntered}" ref="nav" @stackClicked="dropNavMenu()" @nameClicked="refreshIfAtTsd()"/>
+  <NavBar class="nav active" @stackClicked="dropNavMenu()"/>
   <transition name="slide"><MobileNavMenu style="opacity: 1;" v-if="showMobileNavMenu" @linkClicked="dropNavMenu()"/></transition>
   <div id="page" :class="{'non-scroll': showMobileNavMenu}">
     <NuxtPage/>
-    <div><FooterComponent v-if="showFooter" ref="footer"/></div>
+    <div><FooterComponent/></div>
   </div>
 </template>
 
-<script setup lang=ts>
-let nav = ref<HTMLElement | null>(null);
-let footer = ref<HTMLElement | null>(null);
+<script setup lang="ts">
 let siteEntered = ref(false);
+siteEntered.value = true;
 let router = useRouter();
 let showMobileNavMenu = ref(false);
-const route = useRoute();
-
-const showFooter = ref(false) // Initially, showFooter is false
-
-// Computed property to determine if the current route is not the home page
-const isNotHome = computed(() => route.path !== '/')
-
-watch(isNotHome, (newValue) => {
-  if (newValue) {
-    // If the route is not home, wait for a specified timeout before showing the footer
-    setTimeout(() => {
-      showFooter.value = true
-    }, 1000) // Adjust the timeout duration (in milliseconds) as needed
-  } else {
-    // If the route is home, immediately set showFooter to false
-    showFooter.value = false
-  }
-})
-
-onMounted(() => {
-  router.afterEach((to, from) => {
-    setTimeout(() => {
-      siteEntered.value = to.path !== '/';
-    }, 500);
-  });
-  if(window.location.pathname !== '/') {
-    siteEntered.value = true;
-  }
-});
-
-function refreshIfAtTsd() {
-  if (window.location.pathname === '/tsd') {
-    console.log('refreshing');
-    window.location.reload();
-  }
-}
-
 const dropNavMenu = () => showMobileNavMenu.value = !showMobileNavMenu.value;
 </script>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,16 @@
 <template>
-    <LandingPromo />
+    <div id="aboutIndexContainer">
+        <About />
+    </div>
 </template>
+
+<style scoped>
+
+#aboutIndexContainer {
+    width: 100%; 
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
